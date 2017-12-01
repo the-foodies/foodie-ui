512b5469492b68c3a1598f25733ae7549f9e3fad
Zac St Louis <p2kmafia@gmail.com>
2017-11-30 19:19:43 -0800

    added api test for deployment server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,6 +18,7 @@ class App extends React.Component {
           <div>
             <button type="submit"><Link to="/">Home {this.state.asdasd}</Link></button>
             <button type="submit"><Link to="/test">Test Component</Link></button>
+            <button onClick={() => this.props.testApi()}>Test API</button>
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/test" component={TestComponent} />
