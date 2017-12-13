b3c02e96280092ea776ede2e7fabc0e69d403217
Adrian Meza <adrianme213@gmail.com>
2017-12-13 10:29:09 -0800

    [feat] - re-render components to see seed data - add utils function to render sub components

--- src/components/RecipeDetailsPage.jsx
+++ src/components/RecipeDetailsPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid, Row, Col, PageHeader, Label } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import mapImagesToCarouselFormat from '../utils/detailsPage';
+import { mapDetailsToCarouselFormat } from '../utils/detailsPage';
 import TrendingCarousel from './home/TrendingCarousel';
 import NavFilterDetails from './NavFilterDetails';
 import HorizontalScrollBar from './HorizontalScrollBar';
@@ -10,7 +10,11 @@ class RecipeDetailsPage extends React.Component {
   constructor(props) {
     super(props);
     console.log(props);
-    const images = mapImagesToCarouselFormat(props);
+    const images = mapDetailsToCarouselFormat({
+      name: props.name,
+      description: props.name,
+      images: props.imagesRecipes,
+    });
     const {
       name,
       sodium,
@@ -53,10 +57,18 @@ class RecipeDetailsPage extends React.Component {
       name,
       portions,
       recipeHistory,
-      similarRecipes: [{ id: 1, name: 'Test Recipe', imageUrl: 'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto,fl_lossy/wp-cms/uploads/2017/06/i-1-sonic-burger.jpg', linkUrl: '/' },
-        { id: 2, name: 'Test 2', imageUrl: 'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto,fl_lossy/wp-cms/uploads/2017/06/i-1-sonic-burger.jpg', linkUrl: '/' },
-        { id: 3, name: 'Test 3', imageUrl: 'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto,fl_lossy/wp-cms/uploads/2017/06/i-1-sonic-burger.jpg', linkUrl: '/' },
-        { id: 4, name: 'Test 4', imageUrl: 'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto,fl_lossy/wp-cms/uploads/2017/06/i-1-sonic-burger.jpg', linkUrl: '/' },
+      similarRecipes: [{
+        id: 1, name: 'Test Recipe', image_url: 'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto,fl_lossy/wp-cms/uploads/2017/06/i-1-sonic-burger.jpg', linkUrl: '/',
+      },
+      {
+        id: 2, name: 'Test 2', image_url: 'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto,fl_lossy/wp-cms/uploads/2017/06/i-1-sonic-burger.jpg', linkUrl: '/',
+      },
+      {
+        id: 3, name: 'Test 3', image_url: 'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto,fl_lossy/wp-cms/uploads/2017/06/i-1-sonic-burger.jpg', linkUrl: '/',
+      },
+      {
+        id: 4, name: 'Test 4', image_url: 'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto,fl_lossy/wp-cms/uploads/2017/06/i-1-sonic-burger.jpg', linkUrl: '/',
+      },
       ],
     };
   }
@@ -136,6 +148,7 @@ RecipeDetailsPage.propTypes = {
   recipeHistory: PropTypes.string.isRequired,
   directions: PropTypes.array.isRequired,
   ingredients: PropTypes.array.isRequired,
+  imagesRecipes: PropTypes.array.isRequired,
 };
 
 export default RecipeDetailsPage;
--- src/containers/app.js
+++ src/containers/app.js
@@ -4,6 +4,7 @@ import App from '../components/App';
 import * as dispatchModal from '../actions/modal';
 import * as dispatchAuth from '../actions/auth';
 import * as dispatchApi from '../actions/apiRequests/';
+import testRestaurants from '../components/testData/testRestaurants.json';
 
 console.log(dispatchAuth);
 
@@ -12,7 +13,12 @@ function mapStateToProps(state) {
     modal: state.modal,
     auth: state.auth,
     posts: state.app.posts,
+<<<<<<< HEAD
     app: state.app,
+=======
+    curRestaurant: testRestaurants[4], // state.app.curRestaurant
+    curRecipe: testRestaurants[0], // state.app.curRecipe
+>>>>>>> [feat]
   };
 }
 
